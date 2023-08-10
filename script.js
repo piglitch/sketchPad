130f0a0093a8bb11d612102c4b28b6ad394b524b
Avi <avibanerjee2000@gmail.com>
2023-08-11 02:10:37 +0530

    The grid is visible before clicking on choose color

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const gridBox = document.querySelector('.grid-container');
+const boxArray = [];
 
-const getColor = () =>{
+const daBox = () => {
+for (let i = 0; i < 110; i++) {
+    for (let j = 0; j < 60; j++) {   
+        const box = document.createElement('div')
+        box.classList.add('boxes')    
+        gridBox.appendChild(box)
+        boxArray.push(box)
+        };
+    }
+}
+daBox();
 
+
+
+const draw = () => {
     var colorChosen = '';
     colorChosen = document.querySelector('.colorInput').value;
-    //console.log(colorChosen);
     let isColorChangeEnabled = false;
 
-    for (let i = 0; i < 110; i++) {
-        for (let j = 0; j < 60; j++) {   
-            const box = document.createElement('div');
-            box.classList.add('boxes');    
-            gridBox.appendChild(box);
-
-            box.addEventListener('click', ()=> {                    
-                isColorChangeEnabled = !isColorChangeEnabled;                    
-            }); 
-            
-            const draw = () => {           
-                box.addEventListener('mouseover', ()=>{
-                    if(isColorChangeEnabled){
-                        colorChosen = document.querySelector('.colorInput').value;
-                        box.style.backgroundColor = `${colorChosen}`;
-                        console.log(colorChosen);
-                }});
-            }; 
-            draw();
-        };       
-    }
+    for (let i = 0; i < boxArray.length; i++) {
+        const box1 = boxArray[i];
+ 
+        box1.addEventListener('click', () => {                    
+            isColorChangeEnabled = !isColorChangeEnabled;                    
+            }
+        ); 
+                 
+        box1.addEventListener('mouseover', () => {
+        if(isColorChangeEnabled){           
+            box1.style.backgroundColor = `${colorChosen}`;
+            }
+        });
+    }    
 };
+
+
+
+        
 
 
