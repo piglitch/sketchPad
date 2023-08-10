d4e62d6c3bd2aa28c27df4158e153cb727a3efde
Avi <avibanerjee2000@gmail.com>
2023-08-10 17:22:37 +0530

    Now the color does change after selecting a different color

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const gridBox = document.querySelector('.grid-container');
-//const colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "brown", "cyan", "magenta", "teal", "lime", "indigo", "silver", "gold", "maroon"];
 
-let isColorChangeEnabled = false;
+const getColor = () =>{
 
-for (let i = 0; i < 110; i++) {
-    for (let j = 0; j < 60; j++) {   
-        const box = document.createElement('div');
-        box.classList.add('boxes');    
-        gridBox.appendChild(box);
+    var colorChosen = '';
+    colorChosen = document.querySelector('.colorInput').value;
+    //console.log(colorChosen);
+    let isColorChangeEnabled = false;
 
-        box.addEventListener('click', ()=> {                    
-            isColorChangeEnabled = !isColorChangeEnabled;                    
-        }); 
-        
-        const draw = () => {           
-            box.addEventListener('mousemove', ()=>{
-                if(isColorChangeEnabled){
-                    box.style.backgroundColor = 'red';
-            }});
-        }; 
-        draw();
-    };       
-}
+    for (let i = 0; i < 110; i++) {
+        for (let j = 0; j < 60; j++) {   
+            const box = document.createElement('div');
+            box.classList.add('boxes');    
+            gridBox.appendChild(box);
+
+            box.addEventListener('click', ()=> {                    
+                isColorChangeEnabled = !isColorChangeEnabled;                    
+            }); 
+            
+            const draw = () => {           
+                box.addEventListener('mouseover', ()=>{
+                    if(isColorChangeEnabled){
+                        colorChosen = document.querySelector('.colorInput').value;
+                        box.style.backgroundColor = `${colorChosen}`;
+                        console.log(colorChosen);
+                }});
+            }; 
+            draw();
+        };       
+    }
+};
 
 
